43fc9fc5db558111937cea178e4342e2588a1925
anst9000 <coltlaboy@yahoo.se>
2020-09-27 12:14:12 +0200

    Update style and all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/dashboard/Dashboard";
 import Navbar from "./components/layout/Navbar";
+import LogIn from "./components/auth/LogIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
+import ProjectDetails from "./components/projects/ProjectDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
